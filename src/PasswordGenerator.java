7dbc9820105ef2e466a0defc1af9c933efb0a32f
MarcelNota <n3gr1t0.mn@gmail.com>
2026-08-05 23:23:36 +0200

    addition of characters set

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -8,11 +8,33 @@ public class PasswordGenerator {
         String upercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String digits = "0123456789";
         String symbols = "!@#$%^&*()_+?";
+
+        StringBuilder charOptions = new StringBuilder();
+
+        if (useUppercase) {
+            charOptions.append(upercase);
+        }
+
+        if (useLowercase) {
+            charOptions.append(lowercase);
+        }
+
+        if (useNumbers) {
+            charOptions.append(digits);
+        }
+
+        if (useSpecialCharacters) {
+            charOptions.append(symbols);
+        }
+
+
+        String allcharacters = charOptions.toString();
+
         StringBuilder password = new StringBuilder();
         Random rand = new Random();
 
         for(int i = 0 ; i < length; i++ ){
-            char nextLetter = lowercase.charAt(rand.nextInt(lowercase.length()));
+            char nextLetter = allcharacters.charAt(rand.nextInt(allcharacters.length()));
             password.append(nextLetter);
         }
         return password.toString();
